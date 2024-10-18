2f7ade089c1b5f4d4a9f86b7fdbe1c523337933c
Cheng Li Chiang <>
2024-10-18 20:28:45 +0800

    fa

--- app.py
+++ app.py
@@ -1,45 +1,12 @@
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route("/")
+# 函式的裝飾器 (Decorator)：函釋為基礎提供的附加功能
+@app.route("/") 
 def home():
-    return "Hello, World!"
+    return "Hello Flask"
 
-
-@app.route("/get", methods=["GET"])
-def get():
-    return "welcome, you could get someting'"
-
-
-@app.route("/post", methods=["POST"])
-def post():
-    data = request.get_json()
-    return {"whatyoupost": data}
-
-
-@app.route("/double", methods=("GET", "POST"))
-def double():
-
-    if request.method == "GET":
-        return {"getmsg": "you could even deal with them together'"}
-
-    elif request.method == "POST":
-        data = request.get_json()
-        return {"postmsg": data}
-
-
-# register bluepirnt
-from . import blueprint
-app.register_blueprint(blueprint.bp)
-
-from . import libhandler
-app.register_blueprint(libhandler.bp)
-
-# 锁定输出格式为 utf-8
-app.config['JSON_AS_ASCII'] = False
-app.config['JSONIFY_MIMETYPE'] = "application/json;charset=utf-8"
-
-if __name__ == "__main__":
-    app.run(debug=False)
+@app.route("/test")
+def test():
+    return "test"
